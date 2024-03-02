8de8877129e9dd6bb88aedb08e21c54ff79a2a8e
Axelmarting <axelmarting2002@gmail.com>
2024-03-02 13:18:25 -0300

    Arreglando una ruta de acceso

--- primer_parcial.py
+++ primer_parcial.py
@@ -10,6 +10,6 @@ def parse_json(nombre_archivo:str):
         dict_json = json.load(archivo)
     return dict_json["results"]
 
-list_starwars = parse_json(r"C:\Users\Axel\Desktop\Programacion_1\PARCIAL_1\data.json")
+list_starwars = parse_json("data.json")
 
 starwars_app(list_starwars)
